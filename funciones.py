d343e044160f49fed388ff9065f31363bf4e47be
Martin Chalela <tinchochalela@gmail.com>
2020-09-15 10:12:32 -0300

    agregue fib y prime

---
+++ funciones.py
@@ -0,0 +1,18 @@
+
+
+def fib(n):
+    values = [0, 1]
+    while values[-2] < n:
+        values.append(values[-2] + values[-1])
+    return values
+
+
+def prime(n):
+    p = []
+    for num in range(n+1):
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            p.append(num)
+    return p
